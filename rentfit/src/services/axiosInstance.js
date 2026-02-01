7e4d9ec0adb097117374cbccc84d7e8fb4f01880
anshu340 <anshukarki533@gmail.com>
2026-02-01 22:46:38 +0545

    user can see the browser without login

--- rentfit/src/services/axiosInstance.js
+++ rentfit/src/services/axiosInstance.js
@@ -1,37 +1,3 @@
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL + "/api/accounts/",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// // Add this interceptor to automatically include JWT token
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     // Don't add token to login, register, or verify-otp endpoints
-//     const publicEndpoints = ['login/', 'register/', 'verify-otp/', 'clothing/all/'];
-//     const isPublicEndpoint = publicEndpoints.some(endpoint =>
-//       config.url?.includes(endpoint)
-//     ) || (config.url?.match(/clothing\/\d+\/?$/)); // Match clothing/<id>/ but not clothing/my/
-
-//     if (!isPublicEndpoint) {
-//       const token = localStorage.getItem("access_token");
-//       if (token) {
-//         config.headers.Authorization = `Bearer ${token}`;
-//       }
-//     }
-
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default axiosInstance;
-
 import axios from "axios";
 
 const axiosInstance = axios.create({
@@ -44,12 +10,19 @@ const axiosInstance = axios.create({
 // Add this interceptor to automatically include JWT token
 axiosInstance.interceptors.request.use(
   (config) => {
-    // Simplified: Always add the token if it exists in localStorage.
-    // Guests won't have a token, so their requests will correctly be unauthenticated.
-    // Logged-in users will correctly send their token for all requests.
-    const token = localStorage.getItem("access_token") || localStorage.getItem("authToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    // List of explicitly public endpoints that don't need a token
+    const publicEndpoints = ['login/', 'register/', 'verify-otp/', 'clothing/all/'];
+    const isPublic = publicEndpoints.some(endpoint =>
+      config.url === endpoint || config.url?.endsWith('/' + endpoint)
+    ) || (config.url?.match(/clothing\/\d+\/?$/));
+
+    // For all other requests, attempt to add the token
+    if (!isPublic) {
+      const token = localStorage.getItem("access_token") || localStorage.getItem("authToken");
+      // Careful check for both null value and "null" string
+      if (token && token !== "null" && token !== "undefined") {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     }
 
     return config;
